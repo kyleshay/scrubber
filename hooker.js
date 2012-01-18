100af99378bace2e9d78ad5ee59bb778b9e59a50
Kyle Shay <kyle.shay@ymail.com>
2012-01-18 09:28:25 -0500

    added init files

---
+++ hooker.js
@@ -0,0 +1,64 @@
+/*
+Copyright (C) 2012 kyle.shay
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+var hooker = {
+	on: function(evnt, elem, func) {
+		if(typeof elem == 'string')
+			elem = document.getElementById(elem);
+		if(elem == null)
+			return;
+		if (elem.addEventListener) {
+			if(evnt == 'mousewheel')
+				elem.addEventListener('DOMMouseScroll',func,false);
+			elem.addEventListener(evnt,func,false);
+		} else if (elem.attachEvent) {
+			return elem.attachEvent('on'+evnt, func);
+		}
+		else window.alert('something failed when you did that. good job!');
+	},
+	off: function(evnt, elem, func) {
+		if(typeof elem == 'string')
+			elem = document.getElementById(elem);
+		if(elem == null)
+			return;
+		if (elem.removeEventListener) {
+			if(evnt == 'mousewheel')
+				elem.removeEventListener('DOMMouseScroll',func,false);
+			elem.removeEventListener(evnt,func,false);
+		} else if (elem.detachEvent) {
+			return elem.detachEvent('on'+evnt, func);
+		}
+		else window.alert('something failed when you did that. good job!');
+	},
+	fire: function(evnt, elem) {
+		if(typeof elem == 'string')
+			elem = document.getElementById(elem);
+		if(elem == null)
+			return;
+		var evt = document.createEvent('MouseEvents');
+		evt.initEvent(evnt, true, true);
+		if (document.createEvent) {
+			elem.dispatchEvent(evt);
+		} else {
+			elem.fireEvent(evt.eventType, evt);
+		}
+	}
+}
---
+++ scrubber.js
@@ -0,0 +1,136 @@
+/*
+Copyright (C) 2012 kyle.shay
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+of the Software, and to permit persons to whom the Software is furnished to do
+so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+var scroll = {
+	_offsetY: 0,
+	_startY: 0,
+	_scrollY: 0,
+	area:null,
+	content:null,
+	bar:null,
+	scrub:null,
+	contentH:0,
+	barH:0,
+	scrubH:0,	
+	initPosition:0,
+	moveVal:0,	
+	initialize: function() {
+		scroll.computeDimensions();
+		scroll.setScrub();
+		scroll.setContent();					
+		
+		hooker.on('mousedown', scroll.scrub, scroll.activate);
+		hooker.on('mousewheel', scroll.area, scroll.slide);
+	},
+	activate: function(e) {
+		hooker.on('mousemove', document, scroll.slide);
+		hooker.on('mouseup', document, scroll.deactivate);
+		
+		scroll.scrub.style.backgroundColor	='#797979';
+		scroll.scrub.style.borderLeft		='1px solid #6C6C6C';
+		scroll.scrub.style.borderTop		='1px solid #5E5E5E';
+		scroll.scrub.style.borderBottom		='1px solid #ADADAD';
+		scroll.scrub.style.borderRight		='1px solid #ADADAD';
+		
+		scroll._offsetY = scroll.getTop(scroll.scrub);
+		scroll._startY = (e.pageY===undefined?e.clientY +  (document.compatMode=="CSS1Compat"?document.documentElement: document.body).scrollTop:e.pageY) + scroll.initPosition;
+		document.body.onselectstart = document.body.onmousedown = function() { return false; };
+	},
+	slide: function(e) {
+		if(scroll.bar.style.visibility == 'hidden') return;		
+		
+		var position = scroll._offsetY+(e.pageY===undefined?e.clientY +  (document.compatMode=="CSS1Compat"?document.documentElement: document.body).scrollTop:e.pageY)-scroll._startY;
+		if(e.type == 'mousewheel' || e.type == 'DOMMouseScroll') {
+			var wheelData = e.detail ? e.detail * -1 : e.wheelDelta / 40;
+		
+			scroll._scrollY = scroll._scrollY <= 0?0:scroll._scrollY;
+			scroll._scrollY = scroll._scrollY >= scroll.barH-scroll.scrubH?scroll.barH-scroll.scrubH:scroll._scrollY;			
+			
+			position = scroll._scrollY -= wheelData*5;
+			scroll.cancelEvent(e);
+		}
+		position = position <= 0?0:position;
+		position = position >= scroll.barH-scroll.scrubH?scroll.barH-scroll.scrubH:position;
+		
+		scroll.scrub.style.top = (position) + 'px';
+		scroll.content.style.marginTop = ((scroll.initPosition-scroll.getTop(scroll.scrub))*scroll.moveVal) + 'px';
+	},
+	deactivate: function(e) {
+		hooker.off('mousemove', document, scroll.slide);
+
+		scroll._scrollY = scroll.scrub.style.top.replace('px','');
+		scroll.scrub.style.backgroundColor	='#CBCBCB';
+		scroll.scrub.style.borderLeft		='1px solid #B7B7B7';
+		scroll.scrub.style.borderTop		='1px solid #B7B7B7';
+		scroll.scrub.style.borderBottom		='1px solid #CBCBCB';
+		scroll.scrub.style.borderRight		='1px solid #BDBDBD';
+		
+		document.body.onselectstart = document.body.onmousedown = function() { return true; };
+	},
+	setScrub: function() {		
+		if(scroll.contentH > scroll.barH) {
+			var t=scroll.barH/(scroll.contentH/scroll.barH);			
+			scroll.scrub.style.height=(t<20?20:t)+'px';
+			scroll.scrub.style.top='0px';
+			scroll.bar.style.visibility = '';
+			scroll.scrub.style.visibility = '';
+		} else {
+			scroll.bar.style.visibility = 'hidden';
+			scroll.scrub.style.visibility = 'hidden';
+		}
+		scroll.computeDimensions();
+	},
+	setContent: function() {		
+		scroll.content.style.marginTop='0px';
+	},
+	/*Helper functions*/
+	computeDimensions: function() {
+		scroll.scrub=document.getElementById('scrollscrub');
+		scroll.bar=document.getElementById('scrollbar');
+		scroll.content=document.getElementById('scrollcontent');
+		scroll.area=document.getElementById('scrollarea');
+		
+		scroll.barH = scroll.bar.offsetHeight;
+		scroll.contentH = scroll.content.offsetHeight;
+		scroll.scrubH =  scroll.scrub.offsetHeight;
+		
+		scroll.initPosition = scroll.getTop(scroll.area);
+		scroll.moveVal = (scroll.contentH-scroll.barH)/
+							(scroll.barH-scroll.scrubH);		
+	},
+	getTop: function(el) {
+		for (var y=0;
+			el != null;
+			y += el.offsetTop, el = el.offsetParent);
+		return y;
+	},
+	cancelEvent: function(e) {
+		e = e ? e : window.event;
+		if(e.stopPropagation)
+			e.stopPropagation();
+		if(e.preventDefault)
+			e.preventDefault();
+		e.cancelBubble = true;
+		e.cancel = true;
+		e.returnValue = false;
+		return false;
+	}
+};
